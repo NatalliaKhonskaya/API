e382fc37b89a0f28c07c59fac4edd3f65cbfac5e
Yan Khonski <yankhonskiy@gmail.com>
2022-11-13 23:34:18 +0100

    Implement books endpoint

--- src/main/server.js
+++ src/main/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var users = {};
+var books = {};
 
 function readResourceFile(filePath){
   return fs.readFileSync(filePath);
@@ -12,9 +13,15 @@ app.get('/users', function (request, response) {
    response.end(users);
 })
 
+app.get('/books', function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/json'});
+   response.end(books);
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address;
    var port = server.address().port;
    users = readResourceFile("../resources/users.json");
+   books = readResourceFile("../resources/books.json");
    console.log("Example app listening at http://%s:%s", host, port);
 })
